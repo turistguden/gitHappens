659725f8dad4bec6740d38b7b99c22be8255db0b
turistguden <nikoil1@live.dk>
2024-10-24 18:50:08 +0200

    The exe version of hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl
-    return 0;
+    int x;
+    cin >> x;
+    int y = 0;
+    
+    while (x > 1) {
+        if (x % 2 == 0) {
+            x = x / 2;
+        }
+        else {
+            x = 3 * x + 1;
+        }
+        y++;
+
+        cout << y << ":\t" << x << endl;
+    }
+	return 0;
 }
